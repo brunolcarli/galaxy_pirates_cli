9f6304e05aa78956e3c8aef6cd00427ff5fdd6d9
brunolcarli <brunolcarli@gmail.com>
2025-05-03 23:47:29 -0300

    ADD solar system view

--- app.py
+++ app.py
@@ -345,10 +345,46 @@ class GalaxyClient(cmd.Cmd):
     ##################################
 
 
-    def do_universe(self):
-        """ """
-        print('Universe')
-        print('not implemented')
+    def do_universe(self, *args):
+        """
+        Explore the universe galaxies and solar systems
+        """
+        g, ss, p = self.selected_planet
+        solar_system = get_solar_system(g, ss)['data']['solarSystem']
+
+        headers = ['Planet Position', 'Planet ID', 'Planet Name', 'Planet coordinates', 'Gravity Field']
+        rows = []
+        for pos, value in enumerate(solar_system.values()):
+            position = pos + 1
+            coords = f'[{g}, {ss}, {position}]'
+            if value is None:
+                planet_id = ''
+                planet_name = ''
+            else:
+                planet_id = value['id']
+                planet_name = f'{chr(0x1FA90)} ' + value['name']
+
+            rows.append([position, planet_id, planet_name, coords, ''])
+        
+
+        print('---------------------------------------------------------------------------------------')
+        print('                                  GALAXY OVERVIEW                                      ')
+        print('-------------------------------------------------------------------------------------\n')
+
+        print(tabulate(
+            [
+                ['---', '---', '---', '---', f'{chr(0x1F30C)}', '', '   Galaxies   ', '1 to 9', '', '', f'{chr(0x1F30C)}', '---', '---', '---', '---'],
+                ['---', '---', '---', '---', f'{chr(0x2600)}', '', '   Solar Systems   ', '1 to 500', '', '', f'{chr(0x2600)}', '---', '---', '---', '---'],
+            ]
+        ))
+
+        print(tabulate(rows, headers, tablefmt="double_outline"))
+        print('-----------------------------------------------------------------------------------\n')
+
+        options_menu = [['Available Commands' ,chr(0x15CC),"overview", 'explore_galaxy']]
+        menu_headers = ['COMMAND OPTIONS MENU', chr(0x15CA), chr(0x15CA), chr(0x15CA)]
+        print(tabulate(options_menu, menu_headers, tablefmt="simple"))
+        
 
 
     def do_ss(self, coords):
