d8fad192cbebb8f4382d3fffc9b38e7fed62bce3
brunolcarli <brunolcarli@gmail.com>
2025-05-04 05:05:12 -0300

    ADD planet image pictures

--- settings.py
+++ settings.py
@@ -1,3 +1,8 @@
+
+VERSION = '0.0.0'
+
 API_URL = 'http://104.237.1.145:6424/graphql/'
 
-
+PLANET_PALETTE = ['default', 'xterm', 'linuxconsole', 'solarized', 'rxvt', 'tango', 'gruvboxdark']
+LOGO_PALETTE = ['linuxconsole', 'solarized', 'gruvboxdark']
+BEELZEWARE_PALETTE = ['linuxconsole', 'solarized', 'rxvt']
